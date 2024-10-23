429e1ae61d8b408a3e264a60d94247a1035cf6b8
MACKENNADRIES <138457102+MACKENNADRIES@users.noreply.github.com>
2024-10-23 20:33:13 +1000

    added black formatter

--- core/rak/models.py
+++ core/rak/models.py
@@ -4,43 +4,49 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class RandomActOfKindness(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rak_posts')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rak_posts")
 
     STATUS_CHOICES = [
-        ('open', 'Open'),
-        ('claimed', 'Claimed'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed'),
+        ("open", "Open"),
+        ("claimed", "Claimed"),
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
     ]
-    
+
     VISIBILITY_CHOICES = [
-        ('public', 'Public'),
-        ('private', 'Private'),
+        ("public", "Public"),
+        ("private", "Private"),
     ]
-    
+
     POST_TYPE_CHOICES = [
-        ('offer', 'Offer'),
-        ('request', 'Request'),
+        ("offer", "Offer"),
+        ("request", "Request"),
     ]
 
     title = models.TextField()
     description = models.TextField()
-    media = models.FileField(upload_to='rak_media/', blank=True, null=True)
+    media = models.FileField(upload_to="rak_media/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='open')
-    visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES, default='public')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default="open")
+    visibility = models.CharField(
+        max_length=10, choices=VISIBILITY_CHOICES, default="public"
+    )
     post_type = models.CharField(max_length=10, choices=POST_TYPE_CHOICES)
     aura_points = models.PositiveIntegerField(default=10)
     completed_at = models.DateTimeField(null=True, blank=True)
     is_paid_forward = models.BooleanField(default=False)
     post_anonymously = models.BooleanField(default=False)
-    allow_collaborators = models.BooleanField(default=False)  # Optional field to allow multiple claimants
-    collaborators = models.ManyToManyField(User, related_name='rak_collaborators', blank=True)
+    allow_collaborators = models.BooleanField(
+        default=False
+    )  # Optional field to allow multiple claimants
+    collaborators = models.ManyToManyField(
+        User, related_name="rak_collaborators", blank=True
+    )
     aura_points_awarded = models.BooleanField(default=False)
-    # rak_claims = models.ManyToManyField('RAKClaim', related_name='raks', blank=True) # for collaborate 
-
+    # rak_claims = models.ManyToManyField('RAKClaim', related_name='raks', blank=True) # for collaborate
 
     def enable_collaborators(self):
         self.allow_collaborators = True
@@ -51,32 +57,36 @@ class RandomActOfKindness(models.Model):
             self.collaborators.add(user)
             self.save()
         else:
-            raise ValueError("Collaboration not allowed or user is already a collaborator.")
+            raise ValueError(
+                "Collaboration not allowed or user is already a collaborator."
+            )
 
     def claim_rak(self, user):
-        if self.status != 'open':
-            raise ValueError("This RAK cannot be claimed because it has already been claimed or completed.")
-        
+        if self.status != "open":
+            raise ValueError(
+                "This RAK cannot be claimed because it has already been claimed or completed."
+            )
+
         if self.creator == user:
             raise ValueError("You cannot claim your own RAK.")
-        
-        self.status = 'claimed'
+
+        self.status = "claimed"
         self.save()
 
         RAKClaim.objects.create(rak=self, claimant=user)
 
     def complete_rak(self):
-        if self.status == 'completed':
+        if self.status == "completed":
             return
 
-        self.status = 'completed'
+        self.status = "completed"
         self.completed_at = timezone.now()
         self.save()
 
         # Award aura points to the claimant if the RAK was claimed
-        if hasattr(self, 'rak_claim'):
+        if hasattr(self, "rak_claim"):
             claimant_profile = self.rak_claim.claimant.userprofile
-            aura_points_for_claimant = self.aura_points 
+            aura_points_for_claimant = self.aura_points
             claimant_profile.aura_points += aura_points_for_claimant
             claimant_profile.calculate_level()  # Update aura level if necessary
             claimant_profile.save()
@@ -84,9 +94,14 @@ class RandomActOfKindness(models.Model):
     def send_notification(self, message):
         Notification.objects.create(recipient=self.creator, message=message)
 
+
 class RAKClaim(models.Model):
-    rak = models.ForeignKey(RandomActOfKindness, on_delete=models.CASCADE, related_name='claims')  # Change the related_name
-    claimant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rak_claims', null=True)
+    rak = models.ForeignKey(
+        RandomActOfKindness, on_delete=models.CASCADE, related_name="claims"
+    )  # Change the related_name
+    claimant = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="rak_claims", null=True
+    )
     claimed_at = models.DateTimeField(auto_now_add=True)
     details = models.TextField(blank=True, null=True)
     claim_anonymously = models.BooleanField(default=False)
@@ -95,9 +110,10 @@ class RAKClaim(models.Model):
         return f"RAK claimed by {self.claimant.username} on {self.claimed_at}"
 
 
-
 class Notification(models.Model):
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notifications"
+    )
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
@@ -105,9 +121,16 @@ class Notification(models.Model):
     def __str__(self):
         return f"Notification for {self.recipient.username} - {self.message}"
 
+
 class PayItForward(models.Model):
-    original_rak = models.ForeignKey(RandomActOfKindness, on_delete=models.CASCADE, related_name='pay_it_forwards')
-    new_rak = models.OneToOneField(RandomActOfKindness, on_delete=models.CASCADE, related_name='pay_it_forward_instance')
+    original_rak = models.ForeignKey(
+        RandomActOfKindness, on_delete=models.CASCADE, related_name="pay_it_forwards"
+    )
+    new_rak = models.OneToOneField(
+        RandomActOfKindness,
+        on_delete=models.CASCADE,
+        related_name="pay_it_forward_instance",
+    )
     pay_it_forward_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
